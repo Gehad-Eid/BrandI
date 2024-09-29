99c10a747c4d7234407c6937ca2a3dfb6a6dbbc0
gehad1995 <gehad.gehad1995@gmail.com>
2024-09-29 09:42:42 +0300

    Initial Commit

---
+++ Challange1/Challange1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challange1App.swift
+//  Challange1
+//
+//  Created by Gehad Eid on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Challange1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
