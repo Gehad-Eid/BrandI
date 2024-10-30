15b29b048f482562769db77bdc1cf6b5baa55340
sumaiya-thani <sumaiyathani@gmail.com>
2024-10-30 20:12:49 +0300

    "Upade"

---
+++ Challange1/Views/Intro/Onboarding/onboarding1.swift
@@ -0,0 +1,18 @@
+//
+//  onboarding1.swift
+//  Challange1
+//
+//  Created by sumaiya on 30/10/2567 BE.
+//
+
+import SwiftUI
+
+struct onboarding1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    onboarding1()
+}
---
+++ Challange1/Views/Intro/Onboarding/onboarding2.swift
@@ -0,0 +1,28 @@
+//
+//  onboarding1.swift
+//  Challange1
+//
+//  Created by sumaiya on 30/10/2567 BE.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct onboarding1: View {
+   
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        AnimatedImage(name: "AI.gif")
+            .resizable()
+            .frame(width:200,height:200)
+            
+               
+        
+            
+    }
+}
+
+#Preview {
+    onboarding1()
+}
