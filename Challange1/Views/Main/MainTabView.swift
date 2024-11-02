5e40cab3f5dbabda5177b79e11141bcc87cd193a
sumaiya-thani <sumaiyathani@gmail.com>
2024-11-02 07:50:45 +0300

    "UI UPDATES"

--- Challange1/Views/Main/MainTabView.swift
+++ Challange1/Views/Main/MainTabView.swift
@@ -8,48 +8,39 @@
 import SwiftUI
 
 //TODO: make the app load from DB in the splash
+import SwiftUI
 
 struct MainTabView: View {
     @Binding var isAuthenticated: Bool
     @StateObject private var vm = MainViewModel()
-    
+    @State private var selectedTab = 0 // Track the selected tab
+
     var body: some View {
-        TabView {
-//            AgendaView()
+        TabView(selection: $selectedTab) {
             AgendaView()
                 .tabItem {
                     Label("Agenda", systemImage: "house.fill")
                 }
+                .tag(0)
             
             CalenderMainView()
                 .tabItem {
-                    Label("Calendar", systemImage: "calendar")
+                    Label("Calendar", image: "Calendar_icon")
                 }
+                .tag(1)
             
             SettingsView(isAuthenticated: $isAuthenticated)
                 .tabItem {
                     Label("Settings", systemImage: "gearshape.fill")
                 }
-            
-            
-            // for fast accsess , u can delete 'em ---
-//            ProfileView()
-//                .tabItem {
-//                    Label("Profile", systemImage: "person.fill")
-//                }
-            
-//            EvaluatePostView()
-//                .tabItem {
-//                    Label("Evaluate Post", systemImage: "pencil")
-//                }
-            // --------------
+                .tag(2)
         }
+        .accentColor(.babyBlue)
         .task {
             try? await vm.loadCurrentUser()
         }
     }
 }
-
 
 #Preview {
     MainTabView(isAuthenticated: .constant(false))
